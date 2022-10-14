c782e1404cb16ec031524f17d1dc3b80de745c84
Beyondlogan <Beyondlogangameing@gmail.com>
2022-10-14 15:09:44 +1030

    Installer updates

---
+++ OptionalInstaller.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo.
+echo.
+echo Would you Like to install optifine.
+echo.
+echo This is recommended for less lag and a incress in proforrmance BUT NOT NEEDED.
+echo.
+echo.
+
+echo.
+echo.
+echo 1. skip installing optifine and go to the next step.
+echo 2. Install optifine
+echo.
+echo.
+
+
